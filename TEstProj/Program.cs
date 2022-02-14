684b785c73fbc798cb3237d828bcaf1b4c02de6e
Цой Артем <coi_arti@list.ru>
2022-02-14 09:41:41 +0300

    NEw

---
+++ TEstProj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TEstProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!\nHello 123");
+        }
+    }
+}
